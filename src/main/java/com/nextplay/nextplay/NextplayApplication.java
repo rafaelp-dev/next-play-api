d87133d85bde97c2441fd52be98f6b45e858fafc
rafael <pereiraborgesrafael05@gmail.com>
2026-01-04 15:17:17 -0300

    initial commit

---
+++ src/main/java/com/nextplay/nextplay/NextplayApplication.java
@@ -0,0 +1,13 @@
+package com.nextplay.nextplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextplay/nextplay/NextplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextplay.nextplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
