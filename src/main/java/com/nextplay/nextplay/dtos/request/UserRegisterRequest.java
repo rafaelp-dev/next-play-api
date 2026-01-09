80001b8f2da869a6ef1ade8113cc476c4d4b7bfc
rafael <pereiraborgesrafael05@gmail.com>
2026-01-09 15:07:33 -0300

    Refatoração no UserRegisterRequest

--- src/main/java/com/nextplay/nextplay/dtos/request/UserRegisterRequest.java
+++ src/main/java/com/nextplay/nextplay/dtos/request/UserRegisterRequest.java
@@ -1,12 +1,13 @@
 package com.nextplay.nextplay.dtos.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRegisterRequest(
         @NotBlank(message = "O nome de usuário é obrigatório.")
         String username,
 
-        @NotBlank(message = "O email é obrigatório.")
+        @Email(message = "O email é obrigatório.")
         String email,
 
         @NotBlank(message = "A senha é obrigatória.")
