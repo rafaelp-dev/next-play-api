6a038303d6c1cb559b9456b25ee70bc43b43dd5a
rafael <pereiraborgesrafael05@gmail.com>
2026-01-13 09:57:14 -0300

    Adiciona configuração de user details e user role

--- src/main/java/com/nextplay/nextplay/entities/UserEntity.java
+++ src/main/java/com/nextplay/nextplay/entities/UserEntity.java
@@ -1,16 +1,22 @@
 package com.nextplay.nextplay.entities;
 
+import com.nextplay.nextplay.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +31,9 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
@@ -35,5 +44,39 @@ public class UserEntity {
         this.email = email;
         this.password = password;
         this.createdAt = LocalDateTime.now();
+        this.role = UserRole.DEFAULT;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_DEFAULT"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
---
+++ src/main/java/com/nextplay/nextplay/enums/UserRole.java
@@ -0,0 +1,9 @@
+package com.nextplay.nextplay.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN,
+    DEFAULT
+}
