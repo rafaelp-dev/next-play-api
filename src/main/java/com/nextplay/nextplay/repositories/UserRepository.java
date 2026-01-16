0576fab2a37c85787a587c501c28b54165c9fcd8
rafael <pereiraborgesrafael05@gmail.com>
2026-01-16 09:27:44 -0300

    Adiciona serviço de autenticação e configuração de segurança

--- src/main/java/com/nextplay/nextplay/repositories/UserRepository.java
+++ src/main/java/com/nextplay/nextplay/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.nextplay.nextplay.repositories;
 
 import com.nextplay.nextplay.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository <UserEntity, Long> {
 
     boolean existsByEmail(String email);
 
+    UserDetails findByEmail(String email);
 }
